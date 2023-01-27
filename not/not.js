1a2500947368067e25c258f2b75db5a1b36421bd
isaacmiller-dev <123700450+isaacmiller-dev@users.noreply.github.com>
2023-01-27 18:15:49 -0500

    Add files via upload

---
+++ not/not.js
@@ -0,0 +1,11 @@
+const dateset1 = new Date();
+const date = dateset1.getDate(); 
+
+const dateset2 = new Date();
+const month = dateset2.getMonth();
+
+const princebday = date + " " + month;
+
+if (princebday.toString == "26 0") {
+    document.head.innerHTML = "<meta http-equiv='refresh' content='0; url=princebday.site'/>"
+}
